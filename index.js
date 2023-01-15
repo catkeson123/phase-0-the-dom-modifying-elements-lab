144beeb0e64201019d3710910307d317fa1c1517
Calvin Atkeson <calvin.atkeson@gmail.com>
2023-01-15 14:58:39 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const m = document.getElementById("main");
+m.remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+
+newHeader.textContent = "Calvin is the champion";
